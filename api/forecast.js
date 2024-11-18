82645980ea55fed507f445cfbcb76c4ad3188a93
Kimth90 <kth3188@gmail.com>
2024-11-19 01:59:41 +0900

    add3

--- api/forecast.js
+++ api/forecast.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 
 // URL 인코딩된 서비스 키 사용
 const serviceKey = 'cPdGKAsUpOaVmBWNujf8zCL0q+XyzMSMGebwv4/Lt+MJZCz8lOidIVcww3rhbqJ/yO8OLyRi0QJY/imdYx7zSg==';
 
-// 환경 변수에서 장소와 좌표 매핑 데이터 로드
-// Vercel에서 환경 변수로 `LOCATIONS_JSON` 값을 가져옵니다.
-const locations = JSON.parse(process.env.LOCATIONS_JSON);
+// JSON 파일에서 장소와 좌표 매핑 데이터 로드
+const locations = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'locations.json'), 'utf-8'));
 
 // 날짜와 시간을 자동으로 계산하는 함수 (이전 시간대 내림 처리)
 const getCurrentDateTime = () => {
